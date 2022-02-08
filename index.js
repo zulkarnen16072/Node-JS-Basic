f965a38399c71d961a649d27a8dbb08d312497e4
Zulkarnen <zulkarnen1229@gmail.com>
2022-02-08 18:40:30 +0700

    test create object of Tiger

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
+const { Tiger } = require("./classes/Tiger");
+
 console.log('Hello World')
+
 
 const addTwoNum = (a, b) => {
     return a + b;
 } 
+
+const tiger = new Tiger('Zulkarnen');
+console.log(tiger.name)
 
 console.log(addTwoNum(10, 10))
